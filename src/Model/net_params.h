a79e82975014c51dd42ce952f158d58388874513
Flone <flonednb@gmail.com>
2020-02-13 15:13:38 +0500

    Changed 'ping check interval' from 3 to 5.

--- src/Model/net_params.h
+++ src/Model/net_params.h
@@ -23,7 +23,7 @@
 
 
 // Ping / Keepalive
-#define  PING_CHECK_INTERVAL_SEC             3 // also change in the client
+#define  PING_CHECK_INTERVAL_SEC             5 // also change in the client
 // when changing change client's INTERVAL_KEEPALIVE_SEC macro too
 #define  INTERVAL_KEEPALIVE_SEC              20
 #define  MAX_TIMEOUT_TIME_MS                 10000
